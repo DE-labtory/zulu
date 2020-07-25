adffe1ef8695929c05e062ed52457df59f369270
Jun <leebduk@gmail.com>
2020-07-25 14:26:04 +0900

    feat: apply gin as http server

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/DE-labtory/zulu/server"
+)
+
+func main() {
+	s := server.New()
+	if err := s.Run(":8080"); err != nil {
+		panic(fmt.Sprintf("failed to run server: %s", err.Error()))
+	}
+}
