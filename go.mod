d59bc9a78a4d0e6fb438fa3a2082359f4b4ae156
Daehyun Paik <paik@a30a.dev>
2020-08-18 02:21:15 +0900

    feat: implements key

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/ethereum/go-ethereum v1.9.16
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/assert/v2 v2.0.1
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/go-cmp v0.5.1 // indirect
@@ -14,7 +15,6 @@ require (
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/stretchr/testify v1.6.1
 	github.com/syndtr/goleveldb v1.0.1-0.20190923125748-758128399b1d
-	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
 	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
 	golang.org/x/sys v0.0.0-20200724161237-0e2f3a69832c // indirect
 	golang.org/x/text v0.3.3 // indirect
--- keychain/key.go
+++ keychain/key.go
@@ -1,5 +1,14 @@
 package keychain
 
+import (
+	"bytes"
+	"encoding/hex"
+	"errors"
+	"github.com/ethereum/go-ethereum/crypto"
+	"strings"
+)
+
+// Key is essentially a public and private key pair based on ECDSA which uses the secp256k1 curve
 type Key struct {
 	ID         string
 	PrivateKey []byte
@@ -9,6 +18,51 @@ type Key struct {
 type KeyGenerator struct {
 }
 
+func NewKeyGenerator() KeyGenerator {
+	return KeyGenerator{}
+}
+
 func (k *KeyGenerator) Generate() Key {
-	return Key{}
+	keyPair, _ := crypto.GenerateKey()
+	pub := crypto.FromECDSAPub(&keyPair.PublicKey)
+	// Exclude the first byte which indicates if this public key is compressed or uncompressed
+	pub = pub[1:65]
+
+	return Key{ID: DeriveID(pub),
+		PrivateKey: crypto.FromECDSA(keyPair),
+		PublicKey:  pub,
+	}
+}
+
+func DeriveID(pub []byte) string {
+	return hex.EncodeToString(crypto.Keccak256(pub))
+}
+
+func ValidateKey(k Key) error {
+	// Validate if id contains only hex string
+	if _, err := hex.DecodeString(k.ID); err != nil {
+		return err
+	}
+	if len(k.ID) != 64 {
+		return errors.New("length of key id should be 64")
+	}
+	if len(k.PrivateKey) != 32 {
+		return errors.New("length of private key should be 32")
+	}
+	if len(k.PublicKey) != 64 {
+		return errors.New("length of public key should be 64")
+	}
+
+	raw := k.PrivateKey
+	priv, _ := crypto.ToECDSA(raw)
+	pub := crypto.FromECDSAPub(&priv.PublicKey)
+	if !bytes.Equal(k.PublicKey, pub[1:65]) {
+		return errors.New("invalid key pair")
+	}
+
+	id := DeriveID(k.PublicKey)
+	if strings.Compare(k.ID, id) != 0 {
+		return errors.New("invalid key id")
+	}
+	return nil
 }
