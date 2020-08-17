338430bb58083e375423bcff230940d32367e36d
Daehyun Paik <paik@a30a.dev>
2020-08-17 18:06:34 +0900

    refactor: replace deprecated keychain interface

--- account/ethereum/eth/service.go
+++ account/ethereum/eth/service.go
@@ -11,11 +11,10 @@ func NewService() *Service {
 	return nil
 }
 
-func (s *Service) DeriveAccount(signer keychain.Signer) (types.Account, error) {
-	signer.PubKey()
+func (s *Service) DeriveAccount(key keychain.Key) (types.Account, error) {
 	return types.Account{}, nil
 }
 
-func (s *Service) Transfer(signer keychain.Signer, to string, amount uint) (types.Transaction, error) {
+func (s *Service) Transfer(key keychain.Key, to string, amount uint) (types.Transaction, error) {
 	return types.Transaction{}, nil
 }
