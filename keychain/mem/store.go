8645b417a3b30726c33d7684d31095988207b903
Daehyun Paik <paik@a30a.dev>
2020-08-18 02:21:42 +0900

    feat: implement mem store

--- keychain/mem/store.go
+++ keychain/mem/store.go
@@ -1,15 +1,33 @@
 package mem
 
-import "github.com/DE-labtory/zulu/keychain"
+import (
+	"errors"
+	"github.com/DE-labtory/zulu/keychain"
+)
 
 type MemStore struct {
 	keys map[string]keychain.Key
 }
 
-func (ks *MemStore) Store(k keychain.Key) {
-
+func NewMemStore() MemStore {
+	return MemStore{
+		keys: make(map[string]keychain.Key),
+	}
 }
 
-func (ks *MemStore) Get(id string) keychain.Key {
-	return keychain.Key{}
+func (ks *MemStore) Store(k keychain.Key) error {
+	if err := keychain.ValidateKey(k); err != nil {
+		return err
+	}
+
+	ks.keys[k.ID] = k
+	return nil
+}
+
+func (ks *MemStore) Get(id string) (keychain.Key, error) {
+	k, ok := ks.keys[id]
+	if !ok {
+		return k, errors.New("key not found error")
+	}
+	return k, nil
 }
