87030f0faf3aa5d6bc98f0be2e853aefebc731d8
AgwaB <leesd556@gmail.com>
2020-08-22 16:58:37 +0900

    feat: add chain id to ethereum config

--- account/ethereum/config.go
+++ account/ethereum/config.go
@@ -1,17 +1,23 @@
 package ethereum
 
-import "github.com/DE-labtory/zulu/types"
+import (
+	"github.com/DE-labtory/zulu/types"
+	"math/big"
+)
 
 type Params struct {
 	NodeUrl string
+	ChainId *big.Int
 }
 
 var (
 	MainnetParams = Params{
 		NodeUrl: "https://mainnet.infura.io",
+		ChainId: big.NewInt(1),
 	}
 	RopstenParams = Params{
 		NodeUrl: "https://ropsten.infura.io",
+		ChainId: big.NewInt(3),
 	}
 )
 
