3269eaeabddc65db9003f13e62f5e6e230316685
AgwaB <leesd556@gmail.com>
2020-08-22 18:29:45 +0900

    feat: implements ethereum erc20 transfer

--- account/ethereum/erc20/service.go
+++ account/ethereum/erc20/service.go
@@ -1,28 +1,71 @@
 package erc20
 
 import (
-	"encoding/hex"
+	"github.com/DE-labtory/zulu/account/ethereum"
 	"math/big"
 
 	"github.com/DE-labtory/zulu/keychain"
 	"github.com/DE-labtory/zulu/types"
 	"github.com/ethereum/go-ethereum/common"
+	ethTypes "github.com/ethereum/go-ethereum/core/types"
 )
 
 type Service struct {
-	Decimal int
-	address string
+	decimal            int
+	address            string
+	network            types.Network
+	transactionBuilder ethereum.TransactionBuilder
+	client             ethereum.Client
 }
 
-func NewService(decimal int, address string) *Service {
+func NewService(network types.Network, decimal int, address string) *Service {
+	txBuilder := ethereum.NewTransactionBuilder(ethTypes.NewEIP155Signer(ethereum.Supplier[network].ChainId))
+	client := ethereum.NewGethClient(network)
 	return &Service{
-		Decimal: decimal,
-		address: address,
+		decimal:            decimal,
+		address:            address,
+		network:            network,
+		transactionBuilder: *txBuilder,
+		client:             client,
 	}
 }
 
 func (s *Service) Transfer(key keychain.Key, to string, amount string) (types.Transaction, error) {
-	return types.Transaction{}, nil
+	nonce, err := s.client.NonceAt(to)
+	if err != nil {
+		return types.Transaction{}, err
+	}
+
+	gasPrice, err := s.client.SuggestGasPrice()
+	if err != nil {
+		return types.Transaction{}, err
+	}
+
+	payload, err := s.buildErc20TransferData(to, amount)
+	if err != nil {
+		return types.Transaction{}, err
+	}
+
+	rawTx, err := s.transactionBuilder.Build(
+		nonce,
+		gasPrice,
+		ethereum.Supplier[s.network].GasLimit,
+		s.address,
+		big.NewInt(0),
+		payload,
+		key.PrivateKey,
+	)
+
+	if err != nil {
+		return types.Transaction{}, err
+	}
+
+	txHash, err := s.client.SendTransaction(rawTx)
+	if err != nil {
+		return types.Transaction{}, err
+	}
+
+	return types.Transaction{TxHash: txHash}, nil
 }
 
 func (s *Service) GetInfo() types.Coin {
@@ -34,14 +77,11 @@ func getTransferFunctionSignatrue() []byte {
 }
 
 func (s *Service) buildErc20TransferData(to string, amount string) ([]byte, error) {
-	toAddress, err := hex.DecodeString(to)
-	if err != nil {
-		return nil, err
-	}
+	address := common.HexToAddress(to)
 	a := new(big.Int)
 	a.SetString(amount, 10)
 
-	paddedAddress := common.LeftPadBytes(toAddress, 32)
+	paddedAddress := common.LeftPadBytes(address.Bytes(), 32)
 	paddedAmount := common.LeftPadBytes(a.Bytes(), 32)
 
 	var data []byte
---
+++ account/ethereum/erc20/service_test.go
@@ -0,0 +1,43 @@
+package erc20
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"github.com/DE-labtory/zulu/keychain"
+	"github.com/DE-labtory/zulu/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"testing"
+)
+
+func loadDefaultPrivateKey() (*ecdsa.PrivateKey, error) {
+	privateKey, err := crypto.HexToECDSA("9ca9700d14db691586ace71b25fe9973f1d2e0dd874e02e3d2d994ea7594f3e6")
+	if err != nil {
+		return nil, err
+	}
+	return privateKey, nil
+}
+
+func TestService_Transfer(t *testing.T) {
+	service := NewService(
+		types.Ropsten,
+		18,
+		"0x0d4c27c49906208fbd9a9f3a43c63ccbd089f3bf", // EVT
+	)
+
+	privKey, err := loadDefaultPrivateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	key, err := keychain.NewKey(privKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx, err := service.Transfer(key, "0x33ffe564A61d48408b5b8Db0c112e7Cc79d023a5", "1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(tx)
+}
